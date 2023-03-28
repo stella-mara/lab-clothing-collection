a93217849159878f02b4c9f2c3346e27a0e65118
Stella <stellamarawendt@gmail.com>
2023-03-27 22:10:43 -0300

    Pasta layout

---
+++ lab-clothing-collection/src/app/layout/colecao/edicao-colecao/edicao-colecao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edicao-colecao',
+  templateUrl: './edicao-colecao.component.html',
+  styleUrls: ['./edicao-colecao.component.scss']
+})
+export class EdicaoColecaoComponent {
+
+}
---
+++ lab-clothing-collection/src/app/layout/esqueci-senha2/esqueci-senha2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-esqueci-senha2',
+  templateUrl: './esqueci-senha2.component.html',
+  styleUrls: ['./esqueci-senha2.component.scss']
+})
+export class EsqueciSenha2Component {
+
+}

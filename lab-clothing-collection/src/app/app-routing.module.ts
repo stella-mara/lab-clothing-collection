d45cefb1e8da46020ebe9c7ec9a0b12fca49712e
Stella <stellamarawendt@gmail.com>
2023-03-31 14:09:51 -0300

    Realização do login/senha/sidebar/header/submenu

--- lab-clothing-collection/src/app/app-routing.module.ts
+++ lab-clothing-collection/src/app/app-routing.module.ts
@@ -1,7 +1,55 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContentComponent } from './layout/content/content.component';
+import { FullComponent } from './layout/full/full.component';
+import { SubmenuComponent } from './layout/submenu/submenu.component';
+import { EsqueciSenhaComponent } from './pages/esqueci-senha/esqueci-senha.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    {
+     path: '',
+     component: FullComponent,
+     children: [
+
+      {
+        path: 'sidebar',
+        component: SidebarComponent
+      },
+
+      {
+        path: 'header',
+        component: HeaderComponent,
+      }
+
+     ]
+   },
+
+  {
+    path: '',
+    component: ContentComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+
+      {
+        path: 'esquecisenha',
+        component: EsqueciSenhaComponent,
+      },
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- lab-clothing-collection/src/app/app.module.ts
+++ lab-clothing-collection/src/app/app.module.ts
@@ -7,20 +7,18 @@ import { LoginComponent } from './pages/login/login.component';
 import { EsqueciSenhaComponent } from './pages/esqueci-senha/esqueci-senha.component';
 import { EsqueciSenha2Component } from './pages/esqueci-senha2/esqueci-senha2.component';
 import { CadastroUsuarioComponent } from './pages/cadastro-usuario/cadastro-usuario.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { Card1Component } from './pages/dashboard/card1/card1.component';
 import { Card2Component } from './pages/dashboard/card2/card2.component';
 import { Card3Component } from './pages/dashboard/card3/card3.component';
 import { HomeComponent } from './pages/dashboard/home/home.component';
-import { ListagemColecaoComponent } from './pages/listagem-colecao/listagem-colecao.component';
-import { CadastroColecaoComponent } from './pages/cadastro-colecao/cadastro-colecao.component';
-import { EdicaoColecaoComponent } from './pages/edicao-colecao/edicao-colecao.component';
 import { ListagemModelosComponent } from './pages/modelos/listagem-modelos/listagem-modelos.component';
 import { CadastroModelosComponent } from './pages/modelos/cadastro-modelos/cadastro-modelos.component';
 import { EdicaoModelosComponent } from './pages/modelos/edicao-modelos/edicao-modelos.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { SubmenuComponent } from './components/submenu/submenu.component';
+import { ContentComponent } from './layout/content/content.component';
+import { FullComponent } from './layout/full/full.component';
+import { SubmenuComponent } from './layout/submenu/submenu.component';
 
 @NgModule({
   declarations: [
@@ -29,19 +27,17 @@ import { SubmenuComponent } from './components/submenu/submenu.component';
     EsqueciSenhaComponent,
     EsqueciSenha2Component,
     CadastroUsuarioComponent,
-    DashboardComponent,
     Card1Component,
     Card2Component,
     Card3Component,
     HomeComponent,
-    ListagemColecaoComponent,
-    CadastroColecaoComponent,
-    EdicaoColecaoComponent,
     ListagemModelosComponent,
     CadastroModelosComponent,
     EdicaoModelosComponent,
     HeaderComponent,
     SidebarComponent,
+    ContentComponent,
+    FullComponent,
     SubmenuComponent
   ],
   imports: [
